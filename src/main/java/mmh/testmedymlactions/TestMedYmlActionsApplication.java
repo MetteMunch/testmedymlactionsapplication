8f463b00fd04e2fe9121704c1fbbaeb6dbcd69ba
MetteMunch <meha0005@stud.kea.dk>
2025-02-06 10:28:01 +0100

    init commit

---
+++ src/main/java/mmh/testmedymlactions/TestMedYmlActionsApplication.java
@@ -0,0 +1,13 @@
+package mmh.testmedymlactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMedYmlActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMedYmlActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mmh/testmedymlactions/TestMedYmlActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package mmh.testmedymlactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMedYmlActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
